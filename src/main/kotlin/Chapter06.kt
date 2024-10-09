3a53fe67d23f8ced494039abdbe0e448252b05d3
André Wittwer <andre.wittwer@gmail.com>
2024-10-09 16:52:40 +0200

    feat: Chapter 06 - generate sequence

--- src/main/kotlin/Chapter06.kt
+++ src/main/kotlin/Chapter06.kt
@@ -9,5 +9,10 @@ fun main() {
     // No intermediate collections are created!
     val filtered = person.asSequence().map { it.name }.filter { it.startsWith("L") }
     println(filtered.toList())
+
+    val naturalNumbers = generateSequence(0) { it + 1 }
+    val numbersTo100 = naturalNumbers.takeWhile { it <= 100 }
+    println(numbersTo100.sum())
+    // 5050
 }
 
