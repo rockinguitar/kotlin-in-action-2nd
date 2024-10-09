2e81af086ecc9b55a6eac750860d4e46441dbaf0
André Wittwer <andre.wittwer@gmail.com>
2024-10-09 08:58:16 +0200

    feat: finish chapter02

--- src/main/kotlin/Chapter02.kt
+++ src/main/kotlin/Chapter02.kt
@@ -3,7 +3,7 @@ package rocks.aendu.kotlin
 fun main() {
     val myName = "Samuel L. Jackson"
     println("Hello $myName")
-    println("What is max: ${max(2,3)}")
+    println("What is max: ${max(2, 3)}")
     val languages = mutableListOf("Java")
     languages.add("Kotlin")
     println(languages)
@@ -22,7 +22,16 @@ fun main() {
     for (i in 1..100) {
         print(fizzBuzz(i))
     }
+    println()
+    iterateMap()
+    println()
+    binaryRepo()
+    println()
+    listWithIndex()
+    println()
+    checkIfSomethingIsInTheRange()
 }
+
 
 fun max(a: Int, b: Int) = if (a > b) a else b
 
@@ -44,6 +53,7 @@ enum class Color(
     BLUE(0, 0, 255),
     INDIGO(75, 0, 130),
     VIOLET(238, 130, 238);
+
     val rgb = (r * 256 + g) * 256 + b
     fun printColor() = println("$this is $rgb")
 }
@@ -60,14 +70,14 @@ fun getMnemonic(color: Color) =
     }
 
 fun measureColor() = Color.ORANGE
-// as a stand-in for more complex measurement logic
 
-fun getWarmthFromSensor(): String = when(val color = measureColor()) {
+fun getWarmthFromSensor(): String = when (val color = measureColor()) {
     Color.RED, Color.ORANGE, Color.YELLOW -> "warm (red = ${color.r})"
     Color.GREEN -> "neutral (green = ${color.g})"
     Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold (blue = ${color.b})"
 }
 
+// as a stand-in for more complex measurement logic
 fun mix(c1: Color, c2: Color) =
     when (setOf(c1, c2)) {
         setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
@@ -81,4 +91,44 @@ fun fizzBuzz(i: Int) = when {
     i % 3 == 0 -> "Fizz "
     i % 5 == 0 -> "Buzz "
     else -> "$i "
+}
+
+fun iterateMap() {
+    val collection = listOf("red", "green", "blue")
+    for (color in collection) {
+        print("$color ")
+    }
+    // red green blue
+}
+
+fun binaryRepo() {
+
+    val binaryReps = mutableMapOf<Char, String>()
+    for (char in 'A'..'F') {
+        val binary = char.code.toString(radix = 2)
+        binaryReps[char] = binary
+    }
+
+    for ((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
+    // A = 1000001    D = 1000100
+    // B = 1000010    E = 1000101
+    // C = 1000011    F = 1000110
+    // (output split into columns for conciseness)
+}
+
+fun listWithIndex() {
+    val list = listOf("10", "11", "1001")
+    for ((index, element) in list.withIndex()) {
+        println("$index: $element")
+    }
+    // 0: 10
+    // 1: 11
+    // 2: 1001
+}
+
+fun checkIfSomethingIsInTheRange() {
+    println("Kotlin" in "Java".."Scala")
+    // true
 }
